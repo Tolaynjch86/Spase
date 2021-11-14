129d0bcc6fa391aec50e08990b3dda79478a1f86
Tolaynjch86 <92245408+Tolaynjch86@users.noreply.github.com>
2021-11-14 11:36:36 +0300

    first

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1636878851,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/ship-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/fire-sheet0.png",
+		"images/asteroid-sheet0.png",
+		"images/lose-sheet0.png",
+		"images/win-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/explosion-sheet1.png",
+		"images/shield-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
